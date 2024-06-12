f8e5418e1c88dc5bc6db3ecf88a340ceb43fd0b8
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-12 11:03:09 +0000

    OOPS in JS

--- src/app.js
+++ src/app.js
@@ -5,3 +5,6 @@ String.prototype.yell = function(){
 Array.prototype.pop = function(){
     return 'SORRY I WANT THAT ELEMENT, I WILL NEVER POP IT OFF';
 };
+
+const navColor = new Color('carrot',[230,126,34]);
+const logoColor = new Color('emerald',[46,204,133]);
